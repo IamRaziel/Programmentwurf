0e9d6fba76e67fd4f4859f660535e6f9d65480fe
Raziel Engel <raziel.engel@gmx.de>
2022-02-22 14:29:13 +0100

    Added the api and removed the unnecessary things

---
+++ MusicApi/Controllers/Api.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MusicApi.Controllers
+{
+    public class Api
+    {
+        [ApiController]
+        [Route("[controller]")]
+        public class WeatherForecastController : ControllerBase
+        {
+
+            [HttpGet]
+            [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<string>))]
+
+            public IActionResult Get()
+            {
+                return Ok(new string[] { "Hallo", "Welt", "!" });
+            }
+        }
+    }
+}
