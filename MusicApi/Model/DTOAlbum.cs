1790f586d90bf875db1c39a9163fad80ac08d6e6
pascalsimon <pascal.simon@online.de>
2022-02-24 18:51:07 +0100

    Added DTOAlbum

---
+++ MusicApi/Model/DTOAlbum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using MusicApi.Backend.Music;
+
+namespace MusicApi.Model
+{
+    public class DTOAlbum : DTOPlaylist, IAlbum
+    {
+        private string genre;
+        private IList<string> artists;
+
+        public DTOAlbum(string id, string name, string image, IList<ITrack> tracks, string genre, IList<string> artists) : base(id, name, image, tracks)
+        {
+            Genre = genre;
+            Artists = artists;
+        }
+
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+
+        public IList<string> Artists
+        {
+            get { return artists; }
+            set { artists = value; }
+        }
+    }
+}
